8d1321bfd89b2aba547945e0ff9befefd032fb07
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2023-07-20 18:13:14 +0500

    add cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({origin: "*"})
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
 
