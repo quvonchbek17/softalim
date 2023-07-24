c0d544ce54e6a0cb78fea3e7eee35144403b1a11
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2023-07-24 09:27:05 +0500

    migration update

--- src/ormconfig/typeorm.migration.config.ts
+++ src/ormconfig/typeorm.migration.config.ts
@@ -4,13 +4,13 @@ dotenv.config()
 
 export const DataBaseSource = new DataSource({
     type: "postgres",
-    host: "127.0.0.1",
-    port: 5432,
-    password: String("1111"),
-    database: String("softalim"),
-    username: String("postgres"),
+    host: process.env.db_host,
+    port: Number(process.env.db_port),
+    password: process.env.db_password,
+    database: process.env.database,
+    username: process.env.db_username,
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     migrations: [__dirname + '/../migrations/*{.ts,.js}'],
     logging: true,
-    synchronize: false,
+    synchronize: false
 })
